84e1f4d3b17e8406b34b39b4a3bcbcd14f7badb3
snowfall <ammm101046@gmail.com>
2026-02-14 23:21:42 +0300

    refactor(store): remove old unused recent and favorites state

--- stores/useSearchStore.ts
+++ stores/useSearchStore.ts
@@ -1,16 +1,11 @@
 import { ActiveTab } from "@/components/SearchSection/SearchField.types";
-import type { HistoryItem } from "@/components/SearchSection/SearchHistory.types";
 import { create } from "zustand";
 
 export interface SearchStore {
-  recent: HistoryItem[];
-  favorites: HistoryItem[];
   inputValue: string;
   currentTab: ActiveTab;
   isOpen: boolean;
 
-  setRecent: (data: HistoryItem[]) => void;
-  setFavorites: (data: HistoryItem[]) => void;
   setInputValue: (value: string) => void;
   setCurrentTab: (tab: ActiveTab) => void;
   setIsOpen: (value: boolean) => void;
@@ -19,22 +14,16 @@ export interface SearchStore {
 }
 
 export const useSearchStore = create<SearchStore>((set) => ({
-  recent: [],
-  favorites: [],
   inputValue: "",
   currentTab: "recent",
   isOpen: false,
 
-  setRecent: (data: HistoryItem[]) => set({ recent: data }),
-  setFavorites: (data: HistoryItem[]) => set({ favorites: data }),
   setInputValue: (value: string) => set({ inputValue: value }),
   setCurrentTab: (tab: ActiveTab) => set({ currentTab: tab }),
   setIsOpen: (value: boolean) => set({ isOpen: value }),
 
   reset: () =>
     set({
-      recent: [],
-      favorites: [],
       inputValue: "",
       currentTab: "recent",
       isOpen: false,
