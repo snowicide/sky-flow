98e6c9f33304b6b2c2300d51b047adbb5049760f
snowfall <ammm101046@gmail.com>
2026-02-19 16:10:15 +0300

    feat(search): implement search results UI

--- components/SearchSection/SearchResultCity.tsx
+++ components/SearchSection/SearchResultCity.tsx
@@ -14,15 +14,15 @@ export function SearchResultCity({
   const icon = getIconByWeatherCode[code];
 
   return (
-    <li className="flex items-center justify-between">
-      <div className="flex items-center">
-        <Image src={icon} className="w-8 h-8" alt="" />
+    <li className="flex justify-between font-medium mx-2 px-3 py-3 my-3 text-white hover:bg-[hsl(243,23%,30%)] active:opacity-75 rounded-xl cursor-pointer">
+      <div className="flex items-center gap-2">
+        <Image src={icon} className="w-10 h-10" alt="" />
         <span>{`${city}, ${country}`}</span>
       </div>
 
-      <div className="flex items-center">
-        <span>{temperature}</span>
-        <span className="text-white/70">{temperatureUnit}</span>
+      <div className="flex items-center gap-1 font-bold">
+        <span className="text-lg">{temperature.toFixed(1)}</span>
+        <span className="text-white/70 text-base">{temperatureUnit}</span>
       </div>
     </li>
   );
