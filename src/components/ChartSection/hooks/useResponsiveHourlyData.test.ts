331ec22de1f190542c1f2cc6b5e5a4e15b835d92
snowfall <ammm101046@gmail.com>
2026-02-28 19:25:21 +0300

    test(unit): add unit tests for useResponsiveHourlyData

---
+++ src/components/ChartSection/hooks/useResponsiveHourlyData.test.ts
@@ -0,0 +1,65 @@
+import { renderHook } from "@testing-library/react";
+
+import { useDeviceType } from "./useDeviceType";
+import { useResponsiveHourlyData } from "./useResponsiveHourlyData";
+
+vi.mock("./useDeviceType", () => ({
+  useDeviceType: vi.fn(),
+}));
+
+describe("useResponsiveHourlyData", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test.each([
+    {
+      device: {
+        isMobile: true,
+        isTablet: false,
+        isDesk: false,
+        isSmallDesk: false,
+      },
+      data: Array(8).fill({ hour: "0:00", temp: -2 }),
+      expected: 2,
+    },
+    {
+      device: {
+        isMobile: false,
+        isTablet: true,
+        isDesk: false,
+        isSmallDesk: false,
+      },
+      data: Array(8).fill({ hour: "0:00", temp: -2 }),
+      expected: 4,
+    },
+    {
+      device: {
+        isMobile: false,
+        isTablet: false,
+        isDesk: true,
+        isSmallDesk: false,
+      },
+      data: Array(8).fill({ hour: "0:00", temp: -2 }),
+      expected: 8,
+    },
+    {
+      device: {
+        isMobile: false,
+        isTablet: false,
+        isDesk: false,
+        isSmallDesk: true,
+      },
+      data: Array(8).fill({ hour: "0:00", temp: -2 }),
+      expected: 8,
+    },
+  ])(
+    "should return expected length for device",
+    ({ device, data, expected }) => {
+      vi.mocked(useDeviceType).mockReturnValue(device);
+      const { result } = renderHook(() => useResponsiveHourlyData(data));
+
+      expect(result.current.length).toBe(expected);
+    },
+  );
+});
