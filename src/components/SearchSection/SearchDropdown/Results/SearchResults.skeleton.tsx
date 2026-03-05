8e463ed437f24eff64bd6a044a2785daf95a5c76
snowfall <ammm101046@gmail.com>
2026-03-05 18:06:40 +0300

    refactor(search): reduce skeleton items to 4

--- src/components/SearchSection/SearchDropdown/Results/SearchResults.skeleton.tsx
+++ src/components/SearchSection/SearchDropdown/Results/SearchResults.skeleton.tsx
@@ -3,13 +3,13 @@ export function SearchResultsSkeleton() {
     <div
       role="listbox"
       onMouseDown={(e) => e.preventDefault()}
-      className={`absolute -left-5 top-5 sm:top-6 right-0 col-start-1 row-start-2 bg-[hsl(243,27%,20%)] border border-white/10 rounded-xl shadow-[0_10px_12px_black]/25 z-100 mt-1`}
+      className="absolute -left-5 top-5 sm:top-6 right-0 col-start-1 row-start-2 bg-[hsl(243,27%,20%)] border border-white/10 rounded-xl shadow-[0_10px_12px_black]/25 z-100 mt-1"
     >
-      <div className="h-114 lg:h-148.25 space-y-4 flex flex-col justify-center">
-        {[1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
+      <div className="h-65 md:h-80 space-y-4 flex flex-col justify-center">
+        {[1, 2, 3, 4].map((index) => (
           <div
             key={index}
-            className={`bg-[hsl(243,23%,26%)] mx-5 animate-pulse p-1.75 md:p-2.75 rounded-lg border border-white/10`}
+            className={`bg-[hsl(243,23%,24%)] mx-5 animate-pulse p-2.25 md:p-2.75 rounded-lg border border-white/10`}
             style={{ animationDelay: `${index * 0.3}s` }}
           >
             <div className="flex items-center justify-between">
