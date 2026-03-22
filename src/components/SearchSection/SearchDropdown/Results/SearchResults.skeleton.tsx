a82654dca121e9f2f6c67512fcb8cefe90a64dc1
snowfall <ammm101046@gmail.com>
2026-03-22 23:21:05 +0300

    refactor(results): add new `useDeviceType` variable and improve layout
    - add `isDesktopXl` flag to `useDeviceType` for responsiveness
    - replace fixed heights with vertical margins
    - implement responsive margins/paddings for the item container
    - refine `temperature-skeleton` alignment and spacing
    - adjust `border-radius` from `lg` to `md`

--- src/components/SearchSection/SearchDropdown/Results/SearchResults.skeleton.tsx
+++ src/components/SearchSection/SearchDropdown/Results/SearchResults.skeleton.tsx
@@ -1,25 +1,30 @@
+import { useDeviceType } from "@/hooks/useDeviceType";
+
 export function SearchResultsSkeleton() {
+  const { isDesktopXl } = useDeviceType();
+  const skeletonResults = isDesktopXl
+    ? [0, 1, 2, 3, 4, 5, 6, 7]
+    : [0, 1, 2, 3, 4];
+
   return (
     <div
       role="listbox"
       onMouseDown={(e) => e.preventDefault()}
       className="absolute -left-5 top-5 sm:top-6 right-0 col-start-1 row-start-2 bg-[hsl(243,27%,20%)] border border-white/10 rounded-xl shadow-[0_10px_12px_black]/25 z-100 mt-1"
     >
-      <div className="h-65 md:h-80 space-y-4 flex flex-col justify-center">
-        {[1, 2, 3, 4].map((index) => (
+      <div className="space-y-5 my-4.5 flex flex-col justify-center">
+        {skeletonResults.map((index) => (
           <div
             key={index}
-            className={`bg-[hsl(243,23%,24%)] mx-5 animate-pulse p-2.25 md:p-2.75 rounded-lg border border-white/10`}
+            className="bg-[hsl(243,23%,24%)] h-10 mx-2 sm:mx-5 animate-pulse p-1 rounded-lg border border-white/10 flex items-center justify-between"
             style={{ animationDelay: `${index * 0.3}s` }}
           >
-            <div className="flex items-center justify-between">
-              <div className="flex items-center w-6 h-6 md:w-8 md:h-8">
-                <div className="bg-[hsl(243,23%,30%)] ml-1 p-3 md:p-4 rounded-full" />
-                <div className="bg-[hsl(243,23%,30%)] ml-2 px-20 h-5 md:h-6 rounded-lg" />
-              </div>
-
-              <div className="bg-[hsl(243,23%,30%)] ml-2 px-6 h-5 md:h-6 rounded-lg" />
+            <div className="flex items-center w-6 h-6 md:w-8 md:h-8">
+              <div className="bg-[hsl(243,23%,30%)] ml-2 sm:ml-1 lg:ml-2 p-2 md:р-4 rounded-full" />
+              <div className="bg-[hsl(243,23%,30%)] ml-2 px-10 sm:px-15 h-4 rounded-md" />
             </div>
+
+            <div className="bg-[hsl(243,23%,30%)] mr-1 px-6 h-4 rounded-md" />
           </div>
         ))}
       </div>
--- src/hooks/useDeviceType.ts
+++ src/hooks/useDeviceType.ts
@@ -6,6 +6,7 @@ export function useDeviceType(): UseDeviceTypeReturn {
   const isTablet = useMediaQuery({ minWidth: 641, maxWidth: 768 });
   const isDesk = useMediaQuery({ minWidth: 1024 });
   const isSmallDesk = useMediaQuery({ minWidth: 1024, maxWidth: 1150 });
+  const isDesktopXl = useMediaQuery({ minWidth: 1280 });
 
   return useMemo(
     () => ({
@@ -13,8 +14,9 @@ export function useDeviceType(): UseDeviceTypeReturn {
       isTablet,
       isDesk,
       isSmallDesk,
+      isDesktopXl,
     }),
-    [isMobile, isTablet, isDesk, isSmallDesk],
+    [isMobile, isTablet, isDesk, isSmallDesk, isDesktopXl],
   );
 }
 
@@ -23,4 +25,5 @@ interface UseDeviceTypeReturn {
   isTablet: boolean;
   isDesk: boolean;
   isSmallDesk: boolean;
+  isDesktopXl: boolean;
 }
