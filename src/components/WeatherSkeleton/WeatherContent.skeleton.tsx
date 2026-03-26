1b3ce50c064f5460454846c84bcaf9d93dd5d7df
snowfall <ammm101046@gmail.com>
2026-03-26 22:06:11 +0300

    fix(skeleton): increase max width on lg breakpoint
    - apply fix to `WeatherContentSkeleton` component

--- src/components/WeatherSkeleton/WeatherContent.skeleton.tsx
+++ src/components/WeatherSkeleton/WeatherContent.skeleton.tsx
@@ -9,7 +9,7 @@ export default function WeatherContentSkeleton() {
     <>
       <div className="flex flex-col w-full justify-center items-center">
         <div className="flex flex-col w-full xl:w-full max-w-99 sm:max-w-304 lg:gap-4 xl:gap-8 mb-8 lg:mb-0 items-center lg:items-start lg:flex-row">
-          <div className="w-full lg:min-w-150 lg:max-w-154 xl:max-w-200 mx-auto mb-8">
+          <div className="w-full lg:min-w-150 lg:max-w-217.75 xl:max-w-200 mx-auto mb-8">
             <TodayWeatherSkeleton />
             <WeatherDetailsSkeleton />
             <DailyForecastSkeleton />
