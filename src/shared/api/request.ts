9749035df45510a6e3a352532178a0bea56fa571
snowfall <ammm101046@gmail.com>
2026-03-29 00:30:54 +0300

    refactor(shared): implement types and relative path
    - add `Record` to `API_CONFIG` to prevent TS errors
    - return null when 404 status allowed in `request`
    - use relative path in `request.ts` to `error-handler.ts`

--- src/shared/api/request.ts
+++ src/shared/api/request.ts
@@ -1,14 +1,16 @@
-import { throwResponseErrors } from "@shared/api";
+import { throwResponseErrors } from "./error-handler";
 
 export async function request<T>(
   url: string,
   config: RequestInit = {},
   errorCode?: string,
-): Promise<T> {
+  allow404 = false,
+): Promise<T | null> {
   const response = await fetch(url, config);
 
   if (!response.ok) {
     config.signal?.throwIfAborted();
+    if (response.status === 404 && allow404) return null;
     throwResponseErrors(response.status, errorCode);
   }
 
--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,4 +1,4 @@
-export const API_CONFIG = {
+export const API_CONFIG: Record<string, string> = {
   GEO_BASE_URL: "https://geocoding-api.open-meteo.com",
   FORECAST_BASE_URL: "https://api.open-meteo.com",
 } as const;
